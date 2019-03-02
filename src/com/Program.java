94cbc335d3cdc44266db7de7e0ee651d92c54adf
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-03-02 20:41:44 +0200

    supplemented the class Program

--- src/com/Program.java
+++ src/com/Program.java
@@ -1,5 +1,53 @@
+import collection.MyArrayList;
+import collection.MyLinkedList;
+import collection.MyStack;
+
 public class Program {
     public static void main(String[] args) {
+        MyArrayList<String> list = new MyArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("h");
+        list.show();
+        list.remove(4);
+        list.show();
+        System.out.println(list.get(4));
+        list.clear();
+        list.show();
+        System.out.println("--------------------");
 
+        MyLinkedList<String> link = new MyLinkedList<>();
+        link.add("a");
+        link.add("b");
+        link.add("c");
+        link.add("d");
+        link.add("e");
+        link.show();
+        link.remove(4);
+        link.show();
+        System.out.println(link.get(4));
+        link.clear();
+        link.show();
+
+        System.out.println("--------------------");
+        MyStack<String> steck = new MyStack<>();
+        steck.push("a");
+        steck.push("b");
+        steck.push("c");
+        steck.push("d");
+        steck.push("e");
+        steck.push("f");
+        steck.show();
+        System.out.println(steck.size());
+        System.out.println(steck.peek());
+        steck.pop();
+        steck.show();
+        steck.clear();
+        steck.show();
     }
 }
